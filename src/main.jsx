92a6956c6a7d9410ace689ff3d587692505a5d18
W-Carlos <wendcarlos18@hotmail.com>
2023-02-03 11:05:02 -0300

    initial project settings

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Login from './containers/Login'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>
+)
