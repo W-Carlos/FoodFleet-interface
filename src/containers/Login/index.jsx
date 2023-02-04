69f2070b5de3f0cf64ee0c6b3b7fa65708312905
W-Carlos <wendcarlos18@hotmail.com>
2023-02-03 22:10:31 -0300

    Styling the login screen

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 
+import { Container, ImageLogin, ContainerLogin, Logo, H2, Label, Input, SingUpLink, Button } from "./styles";
+import imageLogin from "../../assets/home.jpg"
+
 function Login() {
   return (
-      <h1>Login </h1>
+    <Container>
+      <ImageLogin src={imageLogin} alt="Imagem de login" />
+      <ContainerLogin>
+        <Logo>Food<span>Fleet</span></Logo>
+
+        <H2>Login</H2>
+    
+        <div className="inputs">
+          <Label>Email</Label>
+          <Input type="email" placeholder="Digite seu email" />
+        </div>
+
+        <div className="inputs">
+          <Label>Senha</Label>
+          <Input type="password" placeholder="Digite sua senha" />
+        </div>
+
+        <Button>Entrar</Button>
+
+        <SingUpLink>Não possui conta? <a href="#">Sing up</a></SingUpLink>
+      </ContainerLogin>
+    </Container>
   )
 }
 
---
+++ src/containers/Login/styles.jsx
@@ -0,0 +1,115 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+`
+
+export const ImageLogin = styled.img`
+    width: 50%;
+    object-fit: cover;
+`
+
+export const ContainerLogin = styled.div`
+    height: 100vh;
+    width: 50%;
+
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+
+    padding: 2.5rem 7.5rem;
+
+    .inputs {
+        width: 100%;
+    }
+`
+
+export const Logo = styled.h1`
+
+    font-size: 3.5rem;
+    text-align: center;
+
+    margin-bottom: 5rem;
+    
+    span {
+        color: #FC8019;
+    }
+`
+
+export const H2 = styled.h2`
+    margin-bottom: 1.6rem;
+    
+    text-align: center;
+
+    font-style: normal;
+    font-weight: 600;
+    font-size: 2.4rem;
+    line-height: 2.8rem;
+`
+
+export const Label = styled.label`
+    margin-bottom: 0.3rem;
+    
+    font-style: normal;
+    font-weight: 500;
+    font-size: 1.2rem;
+    line-height: 1.4rem;
+`
+
+export const Input = styled.input`
+    height: 3.8rem;
+    width: 100%;
+    margin-bottom: 2.5rem;
+    padding-left: 1.5rem;
+
+    background: #FFFFFF;
+    box-shadow: 3px 3px 10px rgba(74, 144, 226, 0.19);
+    border-radius: 5px;
+
+    outline: none;
+
+    border: solid 2px #FC8019;
+`
+
+export const Button = styled.button`
+    width: 18.2rem;
+    height: 3.613rem;
+
+    margin-top: 6rem;
+
+    background: #FC8019;
+    color: #fff;
+    border-radius: 20px;
+    border: none;
+    cursor: pointer;
+
+    font-weight: 500;
+    font-size: 1.6rem;
+    line-height: 2.4rem;
+    text-align: center;
+
+    &:hover{
+        opacity: 0.8;
+    }
+
+    &:active{
+        opacity: 0.6;
+    }
+
+`
+
+export const SingUpLink = styled.p`
+    margin-top: 3.2rem;
+    
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1.4rem;
+    line-height: 1.6rem;
+
+    a {
+        color: #000;
+    }
+`
